77b3208f9ba1b9e1f5326bae2c1601d39fa7ffc7
Greg Berenfield <gberenfield@berenfield.com>
2013-05-16 16:36:37 -0400

    hits w/bold txt and selectable

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  ngoo
+//  artemis
 //
 //  Created by Greg Berenfield on 5/14/13.
 //
@@ -18,13 +18,16 @@ struct match {
   char *sloc;
 };
 
-char current[MAX_LINE]; // The current text being typed for search
-int num_tags;           // The file length of the tags file (# of tags)
-int row,col,cursor_row,cursor_col;
-bool showing; // Are we displaying the hunt_list ?
+char current[MAX_LINE];       // The current text being typed for search
+int num_tags;                 // The file length of the tags file (# of tags)
+int row,col,cursor_row,cursor_col,hit_row,hit_col;
+bool showing;                 // Are we displaying the hunt_list ?
 WINDOW *mywin;
 char tags[1000][MAX_LINE];
 struct match matches[100];
+int sel_match;                // currently selected match
+int num_matches;              // number of matches
+char *choice;                 // choice of selection/typing
 
 int flen(char fname[])
 {
@@ -86,12 +89,15 @@ void hunt_current()
   char *k;
 
   clear_matches();
+  sel_match = -1;
+  num_matches = 0;
 
   for (i=0;i<num_tags;i++) {
     if ((k=strcasestr(tags[i],current)) != NULL) {
       matches[j].index=i;
       matches[j].sloc=k;
       j++;
+      num_matches++;
     }
   }
 }
@@ -99,15 +105,25 @@ void hunt_current()
 void show_hits()
 {
   int i=0;
+  char a[128],b[128];
 
   while (matches[i].index > -1) {
-    mvprintw((cursor_row+2+i),cursor_col,"%s",tags[matches[i].index]);
+    memset(a,'\0',128);
+    memset(b,'\0',128);
+    strncpy(a,tags[matches[i].index],matches[i].sloc-tags[matches[i].index]);
+    strcpy(b,&tags[matches[i].index][strlen(a)+strlen(current)]);
+    mvprintw((cursor_row+2+i),cursor_col,"%s",a);
+    attron(A_BOLD);
+    printw("%s",current);
+    attroff(A_BOLD);
+    printw("%s",b);
     ++i;
   }
   showing=TRUE;
   move(cursor_row,cursor_col);
   refresh();
 }
+
 void backspace() {
   noecho();
   noraw();
@@ -117,10 +133,29 @@ void backspace() {
   refresh();
   getsyx(cursor_row,cursor_col);
 }
+void tab_hits_down() {
+  if (sel_match < (num_matches-1)) {
+    sel_match++;
+    if (sel_match==0) {
+      hit_row=cursor_row+1;
+      hit_col=cursor_col;
+    }
+    else {
+      move (hit_row,hit_col);
+      printw(tags[matches[sel_match-1].index]);
+    }
+    move (++hit_row,hit_col);
+    clrtoeol();
+    attron(A_REVERSE);
+    printw(tags[matches[sel_match].index]);
+    attroff(A_REVERSE);
+    move(cursor_row,cursor_col);
+  }
+}
 
 void get_current()
 {
-  int c, i=0;
+  int c,lc , i=0;
 
   raw();
   noecho();
@@ -129,13 +164,17 @@ void get_current()
   while ((c=getch()) != '\n')
   {
     getsyx(cursor_row,cursor_col);
-    if (c=='\t') {
-      clrtobot();
-      if (strlen(current)>1) {
-        hunt_current();
-        show_hits();
+    if (c == '\t') {
+      if (lc == '\t') { tab_hits_down(); }
+      else {
+        clrtobot();
+        if (strlen(current)>1) {
+          hunt_current();
+          show_hits();
+        }
       }
     }
+    else if (c == 'J') tab_hits_down();
     else if (c == 127 || c == 8) { // delete or backspace
       backspace();
       i--;
@@ -155,20 +194,25 @@ void get_current()
     }
   }
   current[i] = '\0';
+  lc = c;
 }
 
 int main(int argc, const char * argv[])
 {
   num_tags=flen(FILENAME);
-  /* char tags[num_tags][MAX_LINE]; */
   get_file(tags);
 
   setup_screen();
 
   get_current();
 
+  if (showing && sel_match>-1) {
+    choice = tags[matches[sel_match].index];
+  }
+  else choice = current;
+
   mvprintw(LINES - 4, 0, "tag: %s", tags[0]);
-  mvprintw(LINES - 3, 0, "You Entered: %s", current);
+  mvprintw(LINES - 3, 0, "You Entered: %s", choice);
   mvprintw(LINES - 2, 0, "Number of tags: %d", num_tags);
   getch();
   endwin();
